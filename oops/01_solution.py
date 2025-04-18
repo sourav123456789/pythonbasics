fea7910183ca22e555fd3c2e5f0997cc2062d236
Sourav Panigrahi <sourav@Souravs-MacBook-Air.local>
2025-04-18 14:12:39 +0530

    updated code

--- oops/01_solution.py
+++ oops/01_solution.py
@@ -1,13 +1,24 @@
 class Car:
+    total_car = 0
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def car_details(self):
         return "car name is " + self.model + " "+ "car brand is " + self.__brand
 
     def get_brand(self):
         return self.__brand
+
+    def fuel_type(self):
+        return "Petrol or diesel"
+
+#     creating a static method
+    @staticmethod
+    def car_description():
+        return "Cars are the mode of Transport"
+
 
 class ElectricCar(Car):
 
@@ -17,12 +28,20 @@ class ElectricCar(Car):
 
 
     def car_details(self):
-        return "car name is " + self.model + " "+ "car brand is " + self.__brand +" " + "battery :" + self.battery_size
+        return "car name is " + self.model + " " + " " + "battery :" + self.battery_size
+
+    def fuel_type(self):
+        return "Fuel is not required."
 
 
 
 my_car = Car("Toyota", "Innova")
 print(my_car.car_details())
-
+print(my_car.fuel_type())
+my_car1 = Car("Toyota", "Innova")
+print(Car.total_car)
+print("---------------------------------------")
 my_electric_car = ElectricCar("100W" , "TATA" , "nano" )
 print(my_electric_car.car_details())
+print(my_electric_car.fuel_type())
+print("car description is: " + Car.car_description())
